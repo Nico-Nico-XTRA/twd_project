eddaa1da09e9963fe6cb70234fb919b74327978b
Nico-Nico <nystroemer@yahoo.dk>
2015-08-13 10:15:27 +0200

    About to change code to make use of generic views!

--- rango/views.py
+++ rango/views.py
@@ -60,4 +60,26 @@ def category(request, category_name_url):
 
 def add_category(request):
     # Get the context from the request
-    context = RequestContext(request)
+    #context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
